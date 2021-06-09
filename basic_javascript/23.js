e90297969a2b59a23d913096596f1cadd1d52c3a
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-06-09 09:55:25 -0400

    exercise 24

---
+++ basic_javascript/23.js
@@ -0,0 +1,5 @@
+// Escaping Literal Quotes in Strings
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+console.log(myStr);
---
+++ basic_javascript/24.js
@@ -0,0 +1,9 @@
+// Quoting Strings with Single Quotes
+
+// before
+
+var myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+
+// after
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
