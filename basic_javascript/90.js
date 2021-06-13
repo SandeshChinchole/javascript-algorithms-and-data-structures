e6ef2894025ae3c9e9e1b23b9f001007b7c168bc
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-06-13 18:00:43 -0400

    exercises 90-91

--- basic_javascript/90.js
+++ basic_javascript/90.js
@@ -1,2 +1,45 @@
 // Manipulating Complex Objects
 
+// var ourMusic = [
+//     {
+//         artist: "a",
+//         title: "a-title",
+//         releaseYear: 2020,
+//         format: ["cd", "cassette", "vinyl"],
+//         gold: true,
+//     },
+//     {
+//         artist: "b",
+//         title: "b-title",
+//         releaseYear: 2020,
+//         formats: ["cd", "cassette", "vinyl"],
+//         gold: true,
+//     },
+//     {
+//         artist: "c",
+//         title: "c-title",
+//         releaseYear: 2021,
+//         formats: ["cd", "cassette", "vinyl"],
+//         gold: false
+//     }
+// ];
+
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      artist: "Linking Park",
+      title: "Hybrid Theory",
+      release_year: 2000,
+      formats: ["CD", "digital"]
+    }
+  ];
