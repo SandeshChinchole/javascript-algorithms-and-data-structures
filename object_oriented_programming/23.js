79e9c764c26a9f93d79cfe6bb284af1592eb7717
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-10-06 04:57:21 -0400

    exercises 24-26

--- object_oriented_programming/23.js
+++ object_oriented_programming/23.js
@@ -1,2 +1,24 @@
 // Use a Mixin to Add Common Behavior Between Unrelated Objects
 
+let bird = {
+    name: "Donald",
+    numLegs: 2
+};
+
+let boat = {
+    name: "Warrior",
+    type: "race-boat"
+};
+
+
+let glideMixin = function(obj){
+    obj.glide = function(){
+        return "gliding";
+    }
+}
+
+glideMixin(bird);
+glideMixin(boat);
+
+bird.glide();
+boat.glide();
